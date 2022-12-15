8e376e7c9d791834d61ea01db1e719d3f2b0fc73
Thomas Fi <tom.filhol@gmail.com>
2022-12-15 21:09:54 +0100

    :rocket: Responsive Navbar

--- index.php
+++ index.php
@@ -14,16 +14,57 @@ require "database/connect.php";
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Poppins&display=swap">
 </head>
 <header class="font-Inter">
-    <div class="mt-6 flex items-center">
-    <img src="public/logo.png" class="ml-4 w-6/12 lg:w-2/12 lg:ml-16 sm:w-4/12 sm:ml-8" href="index.php">
-    <a href="#" class=" bg-gen-blue hover:bg-blue-800 rounded text-white ml-auto mr-4 text-center px-4 py-2 font-bold lg:mr-16 lg:px-5 lg:py-3 lg:text-2xl sm:mr-8">Télécharger</a>
-    </div>
+<nav class="relative px-4 py-4 flex justify-between items-center bg-white lg:mt-4">
+			<a href="" class="lg:ml-16 lg:mr-auto"><img src="public/logo.png" class="lg:h-12" alt=""></a>
+            
+		<div class="lg:hidden">
+			<button class="navbar-burger flex items-center text-gen-blue p-3">
+				<svg class="block h-8 w-8 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<title>Mobile menu</title>
+					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+				</svg>
+			</button>
+		</div>
+		<ul class="hidden  lg:flex lg:gap-10">
+                <li class="text-2xl font-bold">Accueil</li>
+                <li class="text-2xl font-bold">Blog</li>
+                <li><a href="#" class="bg-gen-blue hover:bg-blue-800 rounded text-white ml-auto mr-4 text-center px-4 py-2 font-bold lg:mr-16 lg:px-5 lg:py-3 lg:text-2xl sm:mr-8">Télécharger</a></li>
+            </ul>
+	</nav>
+	<div class="navbar-menu relative z-50 hidden">
+		<div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
+		<nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
+			<div class="flex items-center mb-8">
+            <a href="" class="w-10/12"><img src="public/logo.png" class="lg:h-12" alt=""></a>
+				<button class="navbar-close mr-0 ml-auto">
+					<svg class="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+					</svg>
+				</button>
+			</div>
+			<div>
+				<ul>
+					<li class="mb-1">
+						<a class="block p-4 text-2xl font-bold" href="#">Accueil</a>
+					</li>
+					<li class="mb-1">
+						<a class="block p-4 text-2xl font-bold" href="#">Blog</a>
+					</li>
+				</ul>
+			</div>
+			<div class="mt-auto">
+				<div class="pt-6">
+					<a class="block px-2 py-3 mb- text-center text-white font-bold bg-gen-blue text 2xl rounded" href="#">Télécharger</a>
+				</div>
+			</div>
+            <script src="script.js"></script>
+		</nav>
 </header>
 <body class="font-Inter">
     <div class="lg:flex lg:items-center lg:mt-16 sm:flex">
         <div class="flex-col mt-16 ml-4 lg:ml-16 sm:ml-8">
-            <h1 class="font-bold text-3xl mr-4 lg:text-6xl lg:w-9/12 sm:w-11/12">Choisir les meilleurs produits d'entretien pour votre santé.</h1>
-            <h3 class="mt-2 text-xl mr-4 lg:text-2xl lg:font-bold lg:mt-12 lg:w-7/12 sm:w-10/12">Une application pour scanner vos produits et trouver des bons conseils pour les utiliser.</h3>
+            <h1 class="font-bold text-3xl mr-4 lg:text-6xl lg:w-11/12 sm:w-11/12">Choisir les meilleurs produits d'entretien pour votre santé.</h1>
+            <h3 class="mt-2 text-xl mr-4 lg:text-2xl lg:font-bold lg:mt-12 lg:w-10/12 sm:w-10/12">Une application pour scanner vos produits et trouver des bons conseils pour les utiliser.</h3>
             <div class="flex mt-6 space-x-4 items-center">
                 <a href="#" class=" bg-gen-blue hover:bg-blue-800 px-3 py-1 font-bold text-white rounded lg:text-xl lg:py-4 lg:w-3/12 lg:text-center border-2 border-gen-blue">Télécharger</a>
                 <a href="#" class="text-gen-blue hover:bg-gen-blue hover:text-white font-bold  border-2 border-gen-blue px-3 py-1 rounded lg:py-4 lg:w-4/12 lg:text-xl lg:text-center">Voir la démo</a>
@@ -72,21 +113,25 @@ require "database/connect.php";
 
     <div class="flex w-12/12 mt-8 bg-gen-blue lg:h-auto">
         <div class="flex-col ml-4 items-center">
-            <img src="public/logoblanc.png" alt="" class="h-8 lg:h-16">
+            <img src="public/logoblanc.png" alt="" class="h-10 lg:h-16">
             <div class="flex flex-row  lg:flex lg:flex-col">
-                <div class="flex-col lg:flex lg:flex-row text-white font-semibold">
+                <div class="flex-col text-white font-semibold gap-8">
                     <h2 class="text-xs">Nous contacter</h2>
                     <h2 class="text-xs">Conditions d'utilisations</h2>
                     <h2 class="text-xs">Règle de confidentalité</h2>
                 </div>
-                <div class="flex h-4 ml-auto mr-4">
-                    <img src="public/fb.png" alt="">
-                    <img src="public/tiktok.png" alt="">
-                    <img src="public/snap.png" alt="">
-                    <img src="public/insta.png" alt="">
-                    <img src="public/tumblr.png" alt="">
+                <div class="flex-col right-0">
+                    <div class="flex h-4 ml-auto mr-4">
+                        <img src="public/fb.png" alt="">
+                        <img src="public/tiktok.png" alt="">
+                        <img src="public/snap.png" alt="">
+                        <img src="public/insta.png" alt="">
+                        <img src="public/tumblr.png" alt="">
+                    </div>
+                <div>
+                    <a href="#" class="border-2 text-white border-white rounded w-">Télécharger</a>
                 </div>
-                <a href="admin.php" class="border-2 text-white border-white rounded w-">se connecter</a>
+            </div>
             </div>
         </div>
     </div>
