4ca5ddb7673f605363a5ebe682ce7337defbb7aa
Thomas Fi <tom.filhol@gmail.com>
2022-12-16 16:19:46 +0100

    correction merge

--- blog.php
+++ blog.php
@@ -26,14 +26,15 @@ require 'database/connect.php';
             $all_articles = $take_all->fetchAll();
 
         foreach($all_articles as $article){?>
-            <div class="flex flex-col align-center mt-12 text-lg w-full lg:w-5/12 category_<?= $article["article_category"]?>">
-                <img src="images/<?=$article['article_image']?>" alt="image" class="max-h-48 m-auto object-cover rounded-xl">
-                <h2 class="text-gen-blue font-bold text-center py-4"><?=$article['article_title']?></h2>
-                <section class="text-justify h-2/4 p-2 lg:p-8"><?=substr($article["article_content"], 0, 250)?>...</section>
-                <a href="article.php?id=<?=$article['article_id']?>" class=" bg-gen-blue hover:bg-blue-800 rounded text-white m-2 lg:m-8 mt-4 text-center py-2 font-bold lg:px-5 lg:py-3 lg:text-2xl">Lire la suite</a>
+            <div class="flex flex-col align-center mt-12 text-lg  lg:w-4/12 border-solid  border-grey rounded-xl lg:border-2 lg:shadow-lg lg:mb-16 category_<?= $article["article_category"]?>">
+                <img src="images/<?=$article['article_image']?>" alt="image" class="rounded-t-xl h-34 lg:h-64  w-12/12">
+                <h2 class="font-bold text-center py-4 text-3xl lg:ml-4 lg:mr-4"><?=$article['article_title']?></h2>
+                <section class="text-justify h-2/4 p-4 lg:p-4"><?=substr($article["article_content"], 0, 250)?>...</section>
+                <a href="article.php?id=<?=$article['article_id']?>" class=" hover:text-blue-900 rounded text-gen-blue lg:m-4 mt-4 text-center py-2 font-bold lg:px-5 lg:py-3 lg:text-xl lg:w-6/12 lg:mr-auto lg:ml-auto">Lire la suite</a>
             </div>
     <?php } ?>
     </section>
+    <?php require 'require/footer.php' ?>
     <script src="categorieArticle.js"></script>
     <script src="knowDevice.js"></script>
 </body>
--- require/footer.php
+++ require/footer.php
@@ -1,23 +1,22 @@
-<div class="flex w-12/12 mt-8 bg-gen-blue lg:h-auto">
-        <div class="flex-col ml-4 items-center pb-2">
-            <img src="public/logoblanc.png" alt="Logo de Mon Expert Propreté" class="h-10 lg:h-16">
-            <div class="flex-col lg:flex-col">
-                <div class= "flex text-white gap-2 mt-2 text-sm text-center lg:text-2xl  lg:text-start lg:font-bold lg:flex-col">
-                    <p>Mentions légales</p>
-                    <p>Contact</p>
-                    <p>Politique</p>
-                    <a href="admin.php">connection</p>
-                </div>
-                <div class="flex mt-4 items-center lg:flex-col">
-                    <a class="bg-white text-gen-blue py-1 px-2 rounded  font-bold lg:text-2xl " href="#">Télécharger</a>
-                    <div class="flex h-4 gap-2 mr-4 ml-auto lg:h-8">
-                        <img src="public/fb.png" alt="Facebook">
-                        <img src="public/tiktok.png" alt="Tiktok">
-                        <img src="public/snap.png" alt="Snapchat">
-                        <img src="public/insta.png" alt="Instagram">
-                        <img src="public/tumblr.png" alt="Trumblr">
-                    </div>
-                </div>
+<footer class="bg-gen-blue py-4 lg:py-8 mt-8">
+    <div class="lg:flex-row lg:items-center flex flex-col">
+        <div class="lg:flex-col lg:ml-4 ml-4">
+            <img src="public/logoblanc.png" class="lg:mb-2 w-6/12" alt="">
+            <div class="flex lg:font-bold text-sm lg:flex-row text-white lg:gap-6 lg:text-xl gap-4">
+                <a href="https://monexpertproprete.com/mentions-legales/">Mentions légales</a>
+                <a href="https://monexpertproprete.com/politique-de-confidentialite/">Règles de confidentalité</a>
+                <a href="admin.php">Connexion</a>
             </div>
         </div>
+        <div class="lg:flex-col lg:gap-4 lg:mr-8 lg:ml-auto flex-row flex ml-4 mr-auto items-center justify-center mt-2 space-x-4">
+            <div class="flex gap-2">
+                <img src="public/fb.png" alt="facebook" class="w-4 lg:w-auto">
+                <img src="public/tiktok.png" alt="tiktok" class="w-4 lg:w-auto">
+                <img src="public/snap.png" alt="snapchat" class="w-4 lg:w-auto">
+                <img src="public/insta.png" alt="instagram" class="w-4 lg:w-auto">
+                <img src="public/tumblr.png" alt="trumblr" class="w-4 lg:w-auto" lg:w-auto>
+            </div>
+            <a href="" class=" lg:py-2 lg:px-4 bg-white rounded text-gen-blue lg:text-2xl font-bold py-1 px-2">Télécharger</a>
+        </div>
     </div>
+</footer>
